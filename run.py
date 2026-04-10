e9751c759cc2246c0ef0e7c1f5409cedfad3f161
Shreedhanya <shreedhanya.is23@bmsce.ac.in>
2026-04-10 14:50:51 +0530

    fixed port binding

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
 from app import create_app
@@ -5,4 +6,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
